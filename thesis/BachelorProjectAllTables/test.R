81af7ba5a4a5119a482692ebea6f340dddf62653
oskareffenberger <johannes.effenberger@t-online.de>
2024-03-11 20:49:47 +0100

    Removed thesis folder

--- thesis/BachelorProjectAllTables/test.R
+++
@@ -1,7 +0,0 @@
-library(metagenomeSeq)
-data1 <- read.table('RData\\table', header = FALSE,  sep = '	',  stringsAsFactors = FALSE)
-
-metaSeqObject = newMRexperiment(data1) 
-metaSeqObject_CSS  = cumNorm( metaSeqObject , p=0.8 )
-OTU_read_count_CSS = MRcounts(metaSeqObject_CSS, norm=TRUE, log=FALSE,sl=1000)
-write.table(OTU_read_count_CSS, file = 'RData\\table_out',sep = "	")
